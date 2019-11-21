41047908d67372910f839944cafa49346d257d5b
Carlos Rivadeneira <carlosrivadeneiraz@gmail.com>
2019-11-21 18:04:58 -0500

    marcado de lista

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const FileListItem = ({ file }) => (
+    <tr className="file-list-item" key={file.id}>
+        <td className="file-name">{file.name}</td>
+    </tr>
+);
+FileListItem.propTypes = {
+    files: PropTypes.object.isRequired
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const FileList = ({ files }) => (
+    <table className="file-list">
+        <tbody>
+            {files.map(file => (
+                <FileListItem key={file.id} file={file} />       
+            ))}
+        </tbody>
+    </table>
+);
+FileList.propTypes = {
+    files: PropTypes.array
+};
+
+const testFiles = [
+    {
+        id: 1,
+        name: 'test1.mp3',
+        blob: '',
+        updated_at: "2016-07-18 21:24:00",
+    },
+    {
+        id: 2,
+        name: 'test2.ogg',
+        blob: '',
+        updated_at: "2017-07-18 21:24:00",
+    },
+];
+
+ReactDOM.render(<FileList files={testFiles} />, document.getElementById('root'));
